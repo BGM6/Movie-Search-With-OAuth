bafdf0bff8ef9d5ccf4fc873094461221241c23a
bguilas <bryanjguilas@gmail.com>
2021-06-25 13:14:47 -0700

    added media query

--- src/components/pages/Social.js
+++ src/components/pages/Social.js
@@ -4,13 +4,13 @@ const Social = () => {
 	return(
 		<div>
 			<a href="https://github.com/BGM6" target="_blank" rel="noreferrer"><i
-				className="fab fa-github text-light fa-2x m-2"/></a>
+				className="fab fa-github text-light fa-2x"/></a>
 			<a href="https://www.linkedin.com/in/bryan-guilas-9a00891ab/" target="_blank" rel="noreferrer"><i
-				className="fab fa-linkedin text-light fa-2x m-2"/></a>
+				className="fab fa-linkedin text-light fa-2x"/></a>
 			<a href="https://wakatime.com/@BGM6" target="_blank" rel="noreferrer"><i
-				className="fab fas fa-keyboard text-light fa-2x m-2"/></a>
+				className="fab fas fa-keyboard text-light fa-2x"/></a>
 			<a href="https://www.instagram.com/guilas03/" target="_blank" rel="noreferrer"><i
-				className="fab fa-instagram text-light fa-2x m-2" /></a>
+				className="fab fa-instagram text-light fa-2x" /></a>
 		</div>
 	);
 }
