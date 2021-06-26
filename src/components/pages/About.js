a5384fc90db4619f0887672ffce56e8b1f96a457
bguilas <bryanjguilas@gmail.com>
2021-06-25 20:54:12 -0700

    added about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,7 +2,26 @@ import React from 'react';
 
 const About = () => {
 	return(
-		<div>About</div>
+
+		<div className="ui centered card">
+			<div className="image"  style={{margin: '1rem'}}>
+				<img
+					src="https://live.staticflickr.com/619/22453011456_f3775b2dcc_b.jpg"
+					alt="avatar"/>
+				<p style={{padding: '5px'}} className="header">Bryan Guilas</p>
+				<div className="meta" style={{padding: '5px'}}>
+					<span className="date">Full Stack Developer</span>
+				</div>
+				<div className="description" style={{padding: '5px'}}>
+					Hello! I'm Bryan and this is my dog Niko!
+				</div>
+			</div>
+			<div className="extra content">
+				<a href="https://www.bryanjguilas.com/" target="_blank" rel="noreferrer">
+					<p> Personal Portfolio</p>
+				</a>
+			</div>
+		</div>
 	);
 }
 
